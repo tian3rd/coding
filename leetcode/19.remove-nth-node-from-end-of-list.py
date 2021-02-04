ed3fb179f4e75b5466496e08186f0a253dd58b3d
tian <tianwu@outlook.com>
2021-02-04 21:54:11 +1100

    use two pointers

---
+++ leetcode/19.remove-nth-node-from-end-of-list.py
@@ -0,0 +1,51 @@
+#
+# @lc app=leetcode id=19 lang=python3
+#
+# [19] Remove Nth Node From End of List
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        # if head.next is None:
+        #     return None
+        # dummyHead = ListNode()
+        # dummyHead.next = h = head
+        # l = 0
+        # while h is not None:
+        #     l += 1
+        #     h = h.next
+        # removed = l - n
+        # if removed == 0:
+        #     return head.next
+        # for i in range(l):
+        #     if i+1 == removed:
+        #         head.next = head.next.next
+        #         return dummyHead.next
+        #     head = head.next
+        
+        # using two pointers to keep track of positions
+        # first: has two parts, n and length-n;
+        # second: has two parts, length-n, n;
+        first = second = head
+        for i in range(n):
+            first = first.next
+        # case: delete the first node
+        if first is None:
+            return head.next
+        # now first has traversed to position n, length-n to go.
+        # first, second traverse together to get to the right position
+        while first.next is not None:
+            first = first.next
+            second = second.next
+        # delete the node
+        second.next = second.next.next
+        return head
+            
+# @lc code=end
+
