b0b7993ae1cde09744519b14d6d6f26930a61b71
tian <tianwu@outlook.com>
2021-02-04 19:39:48 +1100

    work it later

---
+++ leetcode/16.3-sum-closest.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=16 lang=python3
+#
+# [16] 3Sum Closest
+#
+
+# @lc code=start
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        # sort the list first
+        nums = sorted(nums)
+        
+# @lc code=end
+
