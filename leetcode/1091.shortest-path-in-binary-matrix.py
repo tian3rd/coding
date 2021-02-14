53489389f8af155ad09355a02aeaf34fdef4f1a1
tian <tianwu@outlook.com>
2021-02-14 12:29:56 +1100

    try 1091 (later)

---
+++ leetcode/1091.shortest-path-in-binary-matrix.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=1091 lang=python3
+#
+# [1091] Shortest Path in Binary Matrix
+#
+
+# @lc code=start
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        
+# @lc code=end
+
