5b273c5c59cdb61e762c8cbd56e3b16d78bf72fd
tian <tianwu@outlook.com>
2021-02-15 20:58:44 +1100

    add placeholder

---
+++ leetcode/99.recover-binary-search-tree.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=99 lang=python3
+#
+# [99] Recover Binary Search Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def recoverTree(self, root: TreeNode) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        # placeholder
+        
+# @lc code=end
+
