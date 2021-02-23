ee81bf7efaebd0e8890085cbf430423aea4935aa
tian <tianwu@outlook.com>
2021-02-24 02:51:00 +1100

    add alternative solution

--- leetcode/53.maximum-subarray.py
+++ leetcode/53.maximum-subarray.py
@@ -7,12 +7,29 @@
 # @lc code=start
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        # if len(nums) == 1:
+        #     return nums[0]
+        # curmax, talmax = nums[0], nums[0]
+        # for i in range(1, len(nums)):
+        #     curmax = max(curmax+nums[i], nums[i])
+        #     talmax = max(talmax, curmax)
+        # return talmax
+
         if len(nums) == 1:
             return nums[0]
-        curmax, talmax = nums[0], nums[0]
-        for i in range(1, len(nums)):
-            curmax = max(curmax+nums[i], nums[i])
-            talmax = max(talmax, curmax)
-        return talmax
+        # cases where all are negative
+        if max(nums) < 0:
+            return max(nums)
+        # at least one positive
+        tal = 0
+        maxi = 0
+        for i in range(len(nums)):
+            tal += nums[i]
+            maxi = max(maxi, tal)
+            # if tally is neg, start again
+            if tal < 0:
+                tal = 0
+        return maxi
+
 # @lc code=end
 
