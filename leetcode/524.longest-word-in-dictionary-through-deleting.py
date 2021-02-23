56ae3ef9e4dc08095c66391d13c0e1dca284e7a0
tian <tianwu@outlook.com>
2021-02-24 02:10:38 +1100

    add placeholder

---
+++ leetcode/524.longest-word-in-dictionary-through-deleting.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=524 lang=python3
+#
+# [524] Longest Word in Dictionary through Deleting
+#
+
+# @lc code=start
+class Solution:
+    def findLongestWord(self, s: str, d: List[str]) -> str:
+        
+# @lc code=end
+
