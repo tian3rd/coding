db1eb0e8aa247039fb2dc311648f2f8e381d71ef
tian <tianwu@outlook.com>
2021-03-17 20:59:32 +1100

    add placeholders

---
+++ leetcode/481.magical-string.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=481 lang=python3
+#
+# [481] Magical String
+#
+
+# @lc code=start
+class Solution:
+    def magicalString(self, n: int) -> int:
+        '''
+        fail within 10 mins
+        '''
+        
+        
+# @lc code=end
+
