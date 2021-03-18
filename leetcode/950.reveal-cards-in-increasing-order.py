3f68c556a519283cb7e37c1a0cb4d6039b98cc10
tian <tianwu@outlook.com>
2021-03-18 22:57:40 +1100

    add placeholder

---
+++ leetcode/950.reveal-cards-in-increasing-order.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=950 lang=python3
+#
+# [950] Reveal Cards In Increasing Order
+#
+
+# @lc code=start
+class Solution:
+    def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
+        
+# @lc code=end
+
