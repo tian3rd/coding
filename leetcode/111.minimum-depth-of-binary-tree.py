e65793bf8b01d605b7de3af1f6ebc78ccd3e81ac
tian <tianwu@outlook.com>
2021-11-20 07:57:11 +1100

    use bfs for minimum depth of binary tree

--- leetcode/111.minimum-depth-of-binary-tree.py
+++ leetcode/111.minimum-depth-of-binary-tree.py
@@ -13,15 +13,40 @@
 #         self.right = right
 class Solution:
     def minDepth(self, root: TreeNode) -> int:
+        # 
+        # if root is None:
+        #     return 0
+        # if root.left is None and root.right is None:
+        #     return 1
+        # if root.left is not None and root.right is None:
+        #     return 1+self.minDepth(root.left)
+        # if root.left is None and root.right is not None:
+        #     return 1+self.minDepth(root.right)
+        # return 1+min(self.minDepth(root.left), self.minDepth(root.right))
+
+        # using BFS
         if root is None:
             return 0
-        if root.left is None and root.right is None:
-            return 1
-        if root.left is not None and root.right is None:
-            return 1+self.minDepth(root.left)
-        if root.left is None and root.right is not None:
-            return 1+self.minDepth(root.right)
-        return 1+min(self.minDepth(root.left), self.minDepth(root.right))
+        # using list as a simple queue
+        q = [root]
+        # already has root, so depth rtn is 1
+        rtn = 1
+        while q:
+            sz = len(q)
+            for _ in range(sz):
+                # get the first out of the queue
+                cur = q.pop(0)
+                # tell if we get to the target leaf
+                if cur.left is None and cur.right is None:
+                    return rtn
+                # if not, keep to the next level
+                if cur.left is not None:
+                    q.append(cur.left)
+                if cur.right is not None:
+                    q.append(cur.right)
+            # after each for loop of the same depth level, we go deeper
+            rtn += 1
+        return rtn
         
 # @lc code=end
 
