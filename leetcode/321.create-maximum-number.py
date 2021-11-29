f88b95bcc864ed9f4bdde792dd60a351a1691df0
tian <tianwu@outlook.com>
2021-11-29 23:28:11 +1100

    placeholder 321

---
+++ leetcode/321.create-maximum-number.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=321 lang=python3
+#
+# [321] Create Maximum Number
+#
+
+# @lc code=start
+class Solution:
+    def maxNumber(self, nums1: List[int], nums2: List[int], k: int) -> List[int]:
+        '''
+        placeholder: 29/11/2021
+        timelapsed: 7 mins
+        '''
+        
+# @lc code=end
+
